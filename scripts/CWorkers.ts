05d34eb7d0cac10241877b6e2c40f13927beedf4
CathyTsar <tennleston@gmail.com>
2017-06-20 00:32:56 +0300

    Реализовано распарралеливание работы между воркерами (без добавления пользовательского кода)

--- scripts/CWorkers.ts
+++ scripts/CWorkers.ts
@@ -12,19 +12,26 @@ class CWorkers {
     public setThreadNumber(value: number){
         this.m_threadNumber = value;
     }
+     
     public run(){
         var part1: string = this.m_inputCode[0];
         var workerString: string = this.m_inputCode[1];
         var part2: string = this.m_inputCode[2];
-        //Один воркер
-        var workerCreateString = "var blob = new Blob([workerString])\;\nvar blobURL = window.URL.createObjectURL(blob)\;\nvar worker = new Worker(blobURL)\;";
-        var allCode: string = part1 +"\n"+ workerCreateString +"\n"+ part2;
+
         //множество воркеров
         var manyWorkersCreating = "var workerList = []\;\nfor (var i = 0; i < this.m_threadNumber; i++) \{\n    var blob = new Blob([workerString])\;\n    var blobURL = window.URL.createObjectURL(blob)\;\n    var newWorker = new Worker(blobURL)\;\n    workerList.push(newWorker)\;\n}";
-        var callWorkers = "for (var i=0; i<this.m_threadNumber;i++){\n     var worker = workerList[i];\n" +  part2 + "\n\};";
+        var paralInputParameter:string = this.editPostMessageParameter(part2);
+        var callWorkers = "for (var i=0; i<this.m_threadNumber;i++){\n     var worker = workerList[i];\n" +  paralInputParameter + "\n\};";
+
         var manyWorkerAllCode = part1 +"\n"+ manyWorkersCreating +"\n"+ callWorkers;
         alert(manyWorkerAllCode);
-        eval(manyWorkerAllCode);
+        let a = (eval(manyWorkerAllCode));
+        window.console.log(a);
+    }
 
+    private editPostMessageParameter(codeStr:string):string{
+
+        var substr:any = codeStr.match(/\.postMessage\(\w+/gi);
+        return codeStr.replace(substr, substr+"/"+this.m_threadNumber);
     }
 }
