b20dc180772232477edd52f1fba1e8b20b5edf9d
CathyTsar <tennleston@gmail.com>
2017-06-20 04:20:11 +0300

    Добавлен вывод результата в соседнее текстовое поле

--- scripts/CWorkers.ts
+++ scripts/CWorkers.ts
@@ -17,21 +17,58 @@ class CWorkers {
         var part1: string = this.m_inputCode[0];
         var workerString: string = this.m_inputCode[1];
         var part2: string = this.m_inputCode[2];
-
+        var postmessageParam = this.getPostmessageParam(part2);
+        var onmessageCode = this.getOnmessageCode(part2);
         //множество воркеров
+        var insertTextString = "(document.getElementById('result-field')).value = 'Common result: ' + pi;";
         var manyWorkersCreating = "var workerList = []\;\nfor (var i = 0; i < this.m_threadNumber; i++) \{\n    var blob = new Blob([workerString])\;\n    var blobURL = window.URL.createObjectURL(blob)\;\n    var newWorker = new Worker(blobURL)\;\n    workerList.push(newWorker)\;\n}";
         var paralInputParameter:string = this.editPostMessageParameter(part2);
-        var callWorkers = "for (var i=0; i<this.m_threadNumber;i++){\n     var worker = workerList[i];\n" +  paralInputParameter + "\n\};";
+        var callWorkers = "for (var i=0; i<this.m_threadNumber;i++){\n     var worker = workerList[i];\n" +  this.insertHtml(paralInputParameter, insertTextString) + "\n\}\;";
+
+        
 
         var manyWorkerAllCode = part1 +"\n"+ manyWorkersCreating +"\n"+ callWorkers;
         alert(manyWorkerAllCode);
-        let a = (eval(manyWorkerAllCode));
-        window.console.log(a);
+        eval(manyWorkerAllCode);
+        //window.console.log(a);
     }
 
     private editPostMessageParameter(codeStr:string):string{
 
         var substr:any = codeStr.match(/\.postMessage\(\w+/gi);
         return codeStr.replace(substr, substr+"/"+this.m_threadNumber);
+    }
+    private getOnmessageCode(codeStr: string){
+        var subList = codeStr.split(/{|}/);
+        for (var i = 0; i < subList.length; i++){
+            if (subList[i].search(/\.postMessage/gi) != -1) return subList[i+1];
+        }
+    }
+    private getPostmessageParam(codeStr: string){
+        var begin = codeStr.search(/\.postMessage\(\b\w+\b\)/);
+        var paramAndAllString = (codeStr.substring(begin+13));
+        return paramAndAllString.match(/\b\w+\b/);
+    }
+    private codeWithoutOnmessage(codeStr: string){
+        var subList = codeStr.split(/\;/g);
+        var result = "";
+        for (var i = 0; i < subList.length; i++){
+            if ((subList[i].search(/\.onmessage/gi) === -1) && (subList[i].length > 0))
+            { 
+                result+=(subList[i]+";");
+                i = i+2;
+            }
+        }
+        return result;
+    }
+    private insertHtml(codeStr: string, insertText: string){
+        var res = "";
+        var subList = codeStr.split(/\}/);
+
+        for (var i = 0; i < subList.length; i++){
+            res += subList[i];
+            if (subList[i].search(/onmessage/g) != -1) res +=(insertText +"}");
+        }
+        return res;
     }
 }
